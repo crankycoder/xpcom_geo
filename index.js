93e9351fa2e092721b56558a1e4d999b30e811ff
Victor Ng <victor@crankycoder.com>
2016-03-17 13:05:25 -0400

    added test for methods that exist on the xpcom service

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ const {Cc, Ci, Cu, Cr, Cm, components} = require("chrome");
 var geolocation = Cc["@mozilla.org/geolocation;1"].getService(Ci.nsISupports);
 
 console.log("Got Geolocation service: " + geolocation);
+console.log("clearWatch is: " + geolocation.clearWatch);
+console.log("getCurrentPosition is: " + geolocation.getCurrentPosition);
+console.log("watchPosition is: " + geolocation.watchPosition);
+console.log("dummyMethod is: " + geolocation.dummyMethod);
+
